bc3063f38c77a546b7f39a07507f277dad5f2961
Mark Daniels <markdaniels@live.co.uk>
2017-12-06 18:26:45 +0000

    added check calc and scenario outline

--- step_definitions/CheckoutSteps.java
+++ step_definitions/CheckoutSteps.java
@@ -2,9 +2,10 @@ package step_definitions;
 
 import cucumber.api.java.en.*;
 import cucumber.api.PendingException;
+import static org.junit.Assert.*;
 
 import implementation.Checkout;
-import static org.junit.Assert.*;
+
 
 public class CheckoutSteps {
   //instance variable
@@ -12,18 +13,16 @@ public class CheckoutSteps {
   Checkout checkout;
 
 @Given("^the price of a \"(.*?)\" is (\\d+)c$")
-public void thePriceOfAIsC(String name, int price) throws Throwable {
-    bananaPrice = price;
+  public void thePriceOfAIsC(String name, int price) throws Throwable {
+      bananaPrice = price;
 }
-
 @When("^I checkout (\\d+) \"(.*?)\"$")
-public void iCheckout(int itemCount, String itemName) throws Throwable {
-    checkout = new Checkout();
-    checkout.add(itemCount, bananaPrice);
+  public void iCheckout(int itemCount, String itemName) throws Throwable {
+      checkout = new Checkout();
+      checkout.add(itemCount, bananaPrice);
 }
-
 @Then("^the total price should be (\\d+)c$")
-public void theTotalPriceShouldBeC(int total) throws Throwable {
-    assertEquals(total, checkout.total());
+  public void theTotalPriceShouldBeC(int total) throws Throwable {
+      assertEquals(total, checkout.total());
   }
 }
