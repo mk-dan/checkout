899959e40d4dc51dfbe4c1d81cb6b8feb534b5a6
Mark Daniels <markdaniels@live.co.uk>
2017-12-07 14:59:07 +0000

    single, double, separate & combination checkout scenarios with java if-else statements

--- features/checkout.feature
+++ features/checkout.feature
@@ -1,5 +1,10 @@
 Feature: Checkout
 
+Scenario: Checkout a banana
+    Given the price of a "banana" is 40c
+    When I checkout 1 "banana"
+    Then the total price should be 40c
+
 Scenario Outline: Checkout bananas
     Given the price of a "banana" is 40c
     When I checkout <count> "banana"
@@ -16,3 +21,10 @@ Scenario: Two bananas scanned separately
     When I checkout 1 "banana"
     And I checkout 1 "banana"
     Then the total price should be 80c
+
+Scenario: A banana and an apple
+    Given the price of a "banana" is 40c
+    And the price of a "apple" is 25c
+    When I checkout 1 "banana"
+    And I checkout 1 "apple"
+    Then the total price should be 65c
--- step_definitions/CheckoutSteps.java
+++ step_definitions/CheckoutSteps.java
@@ -10,15 +10,24 @@ import implementation.Checkout;
 public class CheckoutSteps {
   //instance variable
   int bananaPrice = 0;
+  int applePrice = 0;
   Checkout checkout = new Checkout();
 
 @Given("^the price of a \"(.*?)\" is (\\d+)c$")
   public void thePriceOfAIsC(String name, int price) throws Throwable {
-      bananaPrice = price;
+      if (name.equals("banana")) {
+        bananaPrice = price;
+        } else {
+        applePrice = price;
+      }
   }
 @When("^I checkout (\\d+) \"(.*?)\"$")
   public void iCheckout(int itemCount, String itemName) throws Throwable {
-      checkout.add(itemCount, bananaPrice);
+      if (itemName.equals("banana")) {
+        checkout.add(itemCount, bananaPrice);
+        } else {
+        checkout.add(itemCount, applePrice);
+      }
   }
 @Then("^the total price should be (\\d+)c$")
   public void theTotalPriceShouldBeC(int total) throws Throwable {
